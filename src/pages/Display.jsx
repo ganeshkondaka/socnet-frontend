5f070bb498136615e111eb307c06cb17ee7a36ed
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-12-25 22:32:20 +0530

    added loading

--- src/pages/Display.jsx
+++ src/pages/Display.jsx
@@ -9,15 +9,16 @@ import {
   FaPhone,
   FaTelegram,
   FaHeart,
-  FaImage,
+  FaSpinner,
 } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { PiFinnTheHumanDuotone } from "react-icons/pi";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Display = () => {
 
   const [links, setlinks] = useState([])
+  const [loading, setloading] = useState(true); // Set initial loading state to true
   const { user_name } = useParams()
 
   const fetch_function = async () => {
@@ -27,8 +28,10 @@ const Display = () => {
       // const response = await axios.get(`http://localhost:3000/getsocials/${user_name}`)
       // console.log('the response', response.data.usersocials)
       setlinks(response.data.usersocials)
+      setloading(false); // Set loading to false after data is fetched
     } catch (error) {
       console.log('error', error)
+      setloading(false); // Set loading to false in case of error
     }
 
   }
@@ -37,6 +40,28 @@ const Display = () => {
     fetch_function();
     // console.log('all user links :', links)
   }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black">
+        <motion.div
+          className="text-zinc-200 text-center text-5xl"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, repeat: Infinity, repeatType: "reverse" }}
+        >
+          <div role="status">
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+
+        </motion.div>
+      </div>
+    );
+  }
 
   const renderIcon = (platform) => {
     switch (platform) {
@@ -64,9 +89,8 @@ const Display = () => {
       <motion.div
         className="text-zinc-200 text-center mt-8 text-[22px] p-2 font-bold"
         initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 2, y: 0 }}
         transition={{ duration: 0.5 }}
-        
       >
         <motion.p
           className="text-center text-5xl"
@@ -114,7 +138,7 @@ const Display = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.4 }}
-      > <a href="/" className="text-[13px] p-2 bg-zinc-900 hover:bg-zinc-800 text-zinc-500 my-2 rounded-lg animate-pulse">make your ' SocNet '</a>
+      > <a href="/" className="text-[13px] p-2  hover:bg-zinc-800 text-zinc-500 my-2 rounded-lg animate-pulse border-2 border-zinc-700 ">Create your ' SocNet '</a>
         <p className="mt-4">© 2025 SocNet. All rights reserved.</p>
         <p className="pb-6">
           made with <FaHeart className="inline"></FaHeart> by{" "}
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 
 export default function Form() {
   const navigate = useNavigate();
+  const [loading, setloading] = useState(false); // Set initial loading state to true
+
   const user_id = localStorage.getItem('user_id');
   const user_name = localStorage.getItem('user_name');
 
@@ -22,19 +24,43 @@ export default function Form() {
 
   const handle_submit = async (e) => {
     e.preventDefault();
-    if(fields.length === 1 || fields[0].platform === "" || fields[0].link === "") {
+    if (fields.length === 0 || fields[0].platform === "" || fields[0].link === "") {
       return alert('Please add atleast one link');
     }
-
+    setloading(true);
     try {
       const response = await axios.post('https://socnet-backend.vercel.app/data', fields);
       // const response = await axios.post('http://localhost:3000/data', fields);
       // console.log('response', response.data);
       navigate(`/display/${user_name}`);
+      setloading(false);
     } catch (error) {
       console.log('error', error);
     }
   };
+
+    if (loading) {
+      return (
+        <div className="min-h-screen flex items-center justify-center bg-black">
+          <motion.div
+            className="text-zinc-200 text-center text-5xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, repeat: Infinity, repeatType: "reverse" }}
+          >
+            <div role="status">
+              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+  
+          </motion.div>
+        </div>
+      );
+    }
+  
 
   return (
     <div className="min-h-screen flex flex-col items-center bg-black text-zinc-200 py-8 px-4 pixel-bg">
@@ -61,7 +87,7 @@ export default function Form() {
                 name="platform names"
                 value={field.platform}
                 onChange={(e) => handleInputChange(index, "platform", e.target.value)}
-                
+
                 className="font-mono block w-full md:w-auto bg-zinc-800 text-zinc-200 border-4 border-zinc-600 rounded-md px-2 py-1 pixel-text hover:border-zinc-400 focus:outline-none focus:ring focus:ring-zinc-600"
               >
                 <option value="" disabled>
@@ -80,13 +106,13 @@ export default function Form() {
                 placeholder="Enter link"
                 value={field.link}
                 onChange={(e) => handleInputChange(index, "link", e.target.value)}
-                
+
                 className="block w-full md:w-full bg-zinc-800 text-zinc-200 border-4 border-zinc-600 rounded-md px-2 py-1 pixel-text hover:border-zinc-400 focus:outline-none focus:ring focus:ring-zinc-600"
               />
             </motion.div>
           ))}
           <motion.button
-          type="button"
+            type="button"
             onClick={handleAddField}
             className="w-full bg-zinc-700 text-zinc-200 font-bold py-2 rounded-lg border-4 border-zinc-500 pixel-text hover:bg-zinc-600 hover:border-zinc-400 transition duration-200"
             whileHover={{ scale: 1.05 }}
